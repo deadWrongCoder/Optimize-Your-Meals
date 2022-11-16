e7179e0a901a25407bbd60fcb400c499ccc1b8a8
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-16 13:03:09 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+prices = {
+  "bread": 50,
+  "potato": 10,
+  "tomato": 30,
+  "cucumber": 15,
+  "meat": 200,
+  "milk": 60
+  }
+recipes = {
+  "simple salad": ["tomato", "cucumber"],
+  "simple garnish": ["potato", "milk"],
+  "main course": ["meat", "potato", "milk"]
+}
