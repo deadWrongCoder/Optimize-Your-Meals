032ecd81c9f0a10d52cf3711407de45eb603498b
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-16 16:06:47 +0300

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -1,20 +1,26 @@
 from data import recipes
 
+
 class Preferences:
-  def __init__(self):
-    self.hated_products = []
-    self.favourite_products = []
+    def __init__(self):
+        self.hated_products = []
+        self.favourite_products = []
 
-  def change_preferences(self):
-    hated_products = input("What's the product do you hate the most? ")
-    self.hated_products.append(hated_products)
-    
+    def change_preferences(self):
+        hated_products = input("What are the products do you hate the most? ").split(", ")
+        self.hated_products.extend(hated_products)
 
-  def find_tasty_recipes(self):
-    recipes_available = []
-    for key in recipes:
-      if self.hated_products[0] in recipes[key]:
-        pass
-      else:  
-        recipes_available.append(key)
-    return recipes_available
+    def find_tasty_recipes(self):
+        recipes_available = []
+        deleted_recipes = []
+        for key in recipes:
+            recipes_available.append(key)
+
+        for i in range(0, len(self.hated_products)):
+            for key, value in recipes.items():
+                if self.hated_products[i] in value and key not in deleted_recipes:
+                    recipes_available.remove(key)
+                    deleted_recipes.append(key)
+
+        return recipes_available
+
