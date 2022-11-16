dc4fa948829955e528ed74a3d446fb1764f57bdc
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-16 13:03:48 +0300

    Create finances.py

---
+++ finances.py
@@ -0,0 +1,22 @@
+from data import prices, recipes
+class Finances:
+  def __init__(self):
+    self.budget = 0
+
+  def change_bugdet(self):
+    number = int(input("What's your budget per meal? "))
+    self.budget = number
+
+  def price_of_recipe(self, key):
+    price = 0
+    for i in range(0, len(recipes[key])):
+      price += prices[recipes[key][i]]
+    return price  
+
+  def find_recipes_available(self):
+    recipes_available = []
+    for key in recipes:
+      if self.price_of_recipe(key) <= self.budget:
+        recipes_available.append(key)
+    return recipes_available
+  
