6ba1e2af178dacebfbb3ba6983c3b0f956162af8
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-16 13:02:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,16 @@
 '''Optimize Your Meals
 taking finances, health, time and food preferences into account'''
 
-prices = {
-  "bread": 50,
-  "potato": 10,
-  "tomato": 30,
-  "cucumber": 15,
-  "meat": 200,
-  "milk": 60
-  }
-recipes = {
-  "simple salad": ["tomato", "cucumber"],
-  "simple garnish": ["potato", "milk"],
-  "main course": ["meat", "potato", "milk"]
-}
+from finances import Finances
+from preferences import Preferences
 
-def price_of_recipe(key):
-  price = 0
-  for i in range(0, len(recipes[key])):
-    price += prices[recipes[key][i]]
-  return price
-  
-def find_recipes_available(bugdet_per_meal):
-  recipes_available = []
-  for key in recipes:
-    if price_of_recipe(key) <= bugdet_per_meal:
-      recipes_available.append(key)
-  return recipes_available
-  
-def find_tasty_recipes(food_preferences):
-  recipes_available = []
-  for key in recipes:
-    if food_preferences in recipes[key]:
-      pass
-    else:  
-      recipes_available.append(key)
-  return recipes_available
-  
 
-budget = int(input("What's your budget per meal? "))
-food_preferences = input("What's the product do you hate the most? ")
-budget_list = find_recipes_available(budget)
-tasty_list = find_tasty_recipes(food_preferences)
+user_finances = Finances()
+user_preferences = Preferences()
+
+user_finances.change_bugdet()
+user_preferences.change_preferences()
+budget_list = user_finances.find_recipes_available()
+tasty_list = user_preferences.find_tasty_recipes()
 result = [value for value in budget_list if value in tasty_list]
 print(", ".join(result))
-  
